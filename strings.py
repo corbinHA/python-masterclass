32c6a48645819263420b7d343d15b2ae7c24ea97
Corbin Armendariz <corbinarmendariz@gmail.com>
2021-04-14 12:31:38 -0500

    small work

--- strings.py
+++ strings.py
@@ -1,2 +1,5 @@
-print('Hello, world!')
-print(1 + 2)
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
