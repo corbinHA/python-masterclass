04a1d5afa9ea39081452ee11a17a9bb97bf5533a
Corbin Armendariz <corbinarmendariz@gmail.com>
2021-04-14 12:47:31 -0500

    string manipulation

---
+++ escapechar.py
@@ -0,0 +1,16 @@
+splitString = "This string has been \nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+print("""The pet shop owner said "No, no, 'e's us,...he's resing". """)
+
+anotherSplitString = """This string has been \
+split over \
+several \
+lines"""
+
+print(anotherSplitString)
--- strings.py
+++ strings.py
@@ -3,3 +3,4 @@ print('Python is fun')
 print("Python's string are easy to use")
 print('We can even include "quotes" in strings')
 print("hello" + " world")
+
